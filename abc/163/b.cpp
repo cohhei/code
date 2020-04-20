549d6640e351807d72c8ef2ba150c02ee9d0db43
Kohei <kohei.kimura@nulab.com>
2020-04-20 19:58:00 +0900

    163

---
+++ abc/163/b.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  rep(i, m) {
+    int a;
+    cin >> a;
+    n -= a;
+  }
+  if (n < 0) n= -1;
+  cout << n << endl;
+  return 0;
+}
