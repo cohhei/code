eb13c309d5b9cd106a075abcc13ccb218897dd71
Kohei <kohei.kimura@nulab.com>
2020-05-01 13:32:12 +0900

    39 dp

--- abc/misc/afirstgrader.cpp
+++ abc/misc/afirstgrader.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
+typedef long long ll;
 
 long long dp[101][21];
 int main() {
@@ -9,24 +10,22 @@ int main() {
   vector<int> a(n);
   rep(i, n) cin >> a[i];
 
-  rep(i, n - 1) rep(j, 20) {
-    // if (i == 0) {
-    if (j == a[i]) {
-      dp[i][j] = 1;
-      // else
-      //   dp[i][j] = 0;
-    } else {
-      dp[i + 1][j + a[i]] = dp[i][j - a[i + 1]] + dp[i][j + a[i + 1]];
+  dp[0][a[0]] = 1;
+  for (int i = 0; i < n - 2; i++) {
+    rep(j, 21) {
+      if (j + a[i] <= 20) dp[i + 1][j + a[i]] += dp[i][j];
+      if (j - a[i] >= 0) dp[i + 1][j - a[i]] += dp[i][j];
     }
   }
-  cout << dp[n - 2][a[n - 1] - 1] << endl;
-  rep(i, n) {
-    rep(j, 20) printf("%3d", dp[i][j]);
-    cout << endl;
-    if (i == n - 2) {
-      rep(j, a[n - 1]) cout << "   ";
-      cout << "^" << endl;
-    }
-  }
+  
+  cout << dp[n - 2][a[n - 1]] << endl;
+  // rep(i, n) {
+  //   rep(j, 20) printf("%3ld", dp[i][j]);
+  //   cout << endl;
+  //   if (i == n - 2) {
+  //     rep(j, a[n - 1]) cout << "   ";
+  //     cout << "^" << endl;
+  //   }
+  // }
   return 0;
 }
