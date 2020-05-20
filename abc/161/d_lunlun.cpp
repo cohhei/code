294eee955a2f02363cfa650bd52cd63dc6c16339
Kohei <kohei.kimura@nulab.com>
2020-05-20 20:42:55 +0900

    abc 161

--- abc/161/d_lunlun.cpp
+++ abc/161/d_lunlun.cpp
@@ -1,55 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib> // abs() for integer
-#include <cmath>
-#include <string> // std::string, std::to_string
-
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
 using ll = long long;
-using p = pair<int, int>;
 
-int main()
-{
+int main() {
   int k;
   cin >> k;
-  int num = 0;
-  vector<ll> l(k);
-  for (ll i = 0; i < 10000; i++)
-  {
-    string str = to_string(i);
-    for (int j = 0; j < str.size() - 1; j++)
-    {
-      cout << (unsigned char)str[j] << endl;
-      cout << (unsigned char)str[j+1] << endl;
-      if (abs((unsigned char)str[j] - (unsigned char)str[j + 1]) > 1)
-      {
-        break;
+  queue<ll> q;
+  vector<ll> l;
+  rep(i, 9) {
+    q.push(i + 1);
+    l.push_back(i + 1);
+  }
+  while (l.size() < k) {
+    ll now = q.front();
+    q.pop();
+    for (int i = -1; i < 2; i++) {
+      int n = now % 10 + i;
+      if (0 <= n && n <= 9) {
+        ll m = now * 10 + n;
+        l.push_back(m);
+        q.push(m);
       }
-      l[num++] = i;
-    }
-
-    // if (i < 10)
-    // {
-    //   l[num++] = i;
-    // }
-    // else //if (abs(i / 10 - i % 10) <= 1)
-    // {
-    //   ll tmp = i;
-    //   for (int j = 5; j < 0; j--)
-    //   {
-    //     tmp = tmp / pow(10, j);
-    //     if (abs(i / pow(10, j) - tmp / pow(10, j - 1)) <= 1)
-    //     {
-    //       l[num++] = i;
-    //     }
-    //   }
-    // }
-    if (num - 1 == k)
-    {
-      cout << i << endl;
-      break;
     }
   }
-
+  cout << l[k - 1] << endl;
   return 0;
 }
