cc2d359ed00f293674cde201947d3d6bc140e45b
Kohei Kimura <kohei.kimura@nulab.com>
2020-06-09 00:03:42 +0900

    abc 130

---
+++ abc/130/e.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+const int INF = INT32_MAX;
+const ll INFL = INT64_MAX;
+const int mod = 1e9 + 7;
+
+template <typename T>
+void print_vec(vector<T> v) {
+  for (T i : v) cout << i << " ";
+  cout << endl;
+}
+
+// auto mod int
+// https://youtu.be/L8grWxBlIZ4?t=9858
+// https://youtu.be/ERZuLAxZffQ?t=4807 : optimize
+// https://youtu.be/8uowVvQ_-Mo?t=1329 : division
+struct mint {
+  ll x;  // typedef long long ll;
+  mint(ll x = 0) : x((x % mod + mod) % mod) {}
+  mint operator-() const { return mint(-x); }
+  mint& operator+=(const mint a) {
+    if ((x += a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator-=(const mint a) {
+    if ((x += mod - a.x) >= mod) x -= mod;
+    return *this;
+  }
+  mint& operator*=(const mint a) {
+    (x *= a.x) %= mod;
+    return *this;
+  }
+  mint operator+(const mint a) const {
+    mint res(*this);
+    return res += a;
+  }
+  mint operator-(const mint a) const {
+    mint res(*this);
+    return res -= a;
+  }
+  mint operator*(const mint a) const {
+    mint res(*this);
+    return res *= a;
+  }
+  mint pow(ll t) const {
+    if (!t) return 1;
+    mint a = pow(t >> 1);
+    a *= a;
+    if (t & 1) a *= *this;
+    return a;
+  }
+
+  // for prime mod
+  mint inv() const { return pow(mod - 2); }
+  mint& operator/=(const mint a) { return (*this) *= a.inv(); }
+  mint operator/(const mint a) const {
+    mint res(*this);
+    return res /= a;
+  }
+};
+
+mint dp0[2005][2005];
+mint dp1[2005][2005];
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  vector<int> s(n + 1), t(m + 1);
+  rep(i, n) cin >> s[i];
+  rep(i, m) cin >> t[i];
+  dp0[0][0] = 1;
+  rep(i, n + 1) rep(j, m + 1) {
+    dp0[i + 1][j] += dp0[i][j];
+    dp1[i][j] += dp0[i][j];
+    dp1[i][j + 1] += dp1[i][j];
+    if (s[i] == t[j]) dp0[i + 1][j + 1] += dp1[i][j];
+  }
+  mint ans = dp1[n][m];
+  cout << ans.x << endl;
+  return 0;
+}
